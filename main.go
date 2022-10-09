3e75c656d001d8cd4cb4465b097b1737ad60f55d
Yuta <y.sugiyama54@gmail.com>
2022-10-09 20:51:25 +0900

    connection test

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// define routes
+	http.HandleFunc("/greet", greet)
+
+	// starting server
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
