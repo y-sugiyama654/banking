7d0814ed39ebb75097b55938d64743e84837cad8
Yuta <y.sugiyama54@gmail.com>
2022-10-10 01:45:25 +0900

    add gorilla/mux library

--- app/app.go
+++ app/app.go
@@ -1,17 +1,30 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func Start() {
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 
 	// define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomer)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomer).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8080", mux))
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "post request received.")
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, vars["customer_id"])
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/y-sugiyama654/banking
 
 go 1.14
+
+require github.com/gorilla/mux v1.8.0
