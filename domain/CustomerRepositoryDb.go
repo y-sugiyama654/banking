3de133245f090b648d84322c6cd27d3f3ccd9362
Yuta <y.sugiyama54@gmail.com>
2022-10-14 22:02:56 +0900

    structured logging

--- domain/CustomerRepositoryDb.go
+++ domain/CustomerRepositoryDb.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/y-sugiyama654/banking/errs"
+	"github.com/y-sugiyama654/banking/logger"
 	"log"
 	"time"
 )
@@ -18,7 +19,7 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, *errs.AppError) {
 
 	rows, err := d.client.Query(findAllSql)
 	if err != nil {
-		log.Println("Error while querying customer table" + err.Error())
+		logger.Error("Error while querying customer table" + err.Error())
 		return nil, errs.NewUnexpectedError("Unexpected database error")
 	}
 
@@ -27,7 +28,7 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, *errs.AppError) {
 		var c Customer
 		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
 		if err != nil {
-			log.Println("Error while scanning customers" + err.Error())
+			logger.Error("Error while scanning customers" + err.Error())
 			return nil, errs.NewUnexpectedError("Unexpected database error")
 		}
 		customers = append(customers, c)
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/y-sugiyama654/banking/app"
+import (
+	"github.com/y-sugiyama654/banking/app"
+	"github.com/y-sugiyama654/banking/logger"
+)
 
 func main() {
+	logger.Info("Starting application.")
 	app.Start()
 }
