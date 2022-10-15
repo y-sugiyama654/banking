ea0686878f81848c71c8d6e3250d6549f6b23d42
Yuta <y.sugiyama54@gmail.com>
2022-10-15 11:25:19 +0900

    Using data transfer objects

--- service/customerService.go
+++ service/customerService.go
@@ -2,19 +2,20 @@ package service
 
 import (
 	"github.com/y-sugiyama654/banking/domain"
+	"github.com/y-sugiyama654/banking/dto"
 	"github.com/y-sugiyama654/banking/errs"
 )
 
 type CustomerService interface {
-	GetAllCustomer(status string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetAllCustomer(status string) ([]dto.CustomerResponse, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, *errs.AppError) {
+func (s DefaultCustomerService) GetAllCustomer(status string) ([]dto.CustomerResponse, *errs.AppError) {
 	if status == "active" {
 		status = "1"
 	} else if status == "inactive" {
@@ -22,11 +23,26 @@ func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer
 	} else {
 		status = ""
 	}
-	return s.repo.FindAll(status)
+	customers, err := s.repo.FindAll(status)
+	if err != nil {
+		return nil, err
+	}
+
+	var response []dto.CustomerResponse
+	for _, c := range customers {
+		response = append(response, c.ToDto())
+	}
+
+	return response, nil
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
+	response := c.ToDto()
+	return &response, nil
 }
 
 func NewCustomerRepository(repository domain.CustomerRepository) DefaultCustomerService {
